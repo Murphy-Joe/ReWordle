0216d6df5f8df33e95e3e15eb5a1f6ebeb6c942b
Joe Murphy <murphcle@gmail.com>
2022-05-24 16:47:23 -0400

    origin chrome extension

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ from services import service
 
 app = FastAPI()
 origins = [
-    "https://www.nytimes.com",
-    "chrome-extension://*"
+    "chrome-extension://hhaiiioodinkofkmbigfcjcbicjglkoi"
 ]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["POST"],
     allow_headers=["*"],
